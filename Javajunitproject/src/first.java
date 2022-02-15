7454f049de6149f4094f8a113c1b374acfeaae42
khatalha <khatalha2015@gmail.com>
2022-02-15 20:13:22 +0530

    first commit

---
+++ Javajunitproject/src/first.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+public class first {
+	@Test
+    public void m1() {
+   	 System.out.println("m1 hello world");
+    }
+	 
+	 @Test
+    public void m2() {
+   	 System.out.println("m2 hello world");
+    }
+}
